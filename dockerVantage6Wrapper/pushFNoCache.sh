476c8e13959a50209588b4d2edee77ededa8bcb0
florian <florian.vandaalen@maastro.nl>
2024-03-12 09:33:05 +0100

    removed old files

--- dockerVantage6Wrapper/pushFNoCache.sh
+++
@@ -1,9 +0,0 @@
-IMAGE=harbor.carrier-mu.src.surf-hosted.nl/carrier/health_ri_demo
-
-docker build --no-cache --build-arg SSH_PRIVATE_KEY="$(cat C:\\Florian\\GIT\\KEY\\id_rsa)" \
-  --build-arg SSH-PUBLIC-KEY="$(cat C:\\Florian\\GIT\\KEY\\id_rsa.pub)" \
-  -t health_ri_demo  .
-
-docker tag health_ri_demo $IMAGE
-
-docker push $IMAGE
